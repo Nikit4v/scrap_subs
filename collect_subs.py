6d0a395b2112a9d603a02f27a957fcc1cf061dba
Nikit4v <Dath.vg@gmail.com>
2022-01-16 19:31:36 +0300

    Update collect_subs.py

--- collect_subs.py
+++ collect_subs.py
@@ -6,7 +6,7 @@ import sys
 from tqdm import tqdm
 
 working_path = pathlib.Path(sys.argv[1]) if len(sys.argv) > 1 else pathlib.Path(".")
-ffmpeg_path = sys.argv[2] if len(sys.argv) > 1 else "ffmpeg"
+ffmpeg_path = sys.argv[2] if len(sys.argv) > 2 else "ffmpeg"
 
 
 processes = []
