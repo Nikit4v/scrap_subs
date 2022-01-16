028b931cb01187652d9df3de55f8a3af06c2db82
Nikit4v <Dath.vg@gmail.com>
2022-01-16 19:16:51 +0300

    Update collect_subs.py

--- collect_subs.py
+++ collect_subs.py
@@ -6,13 +6,14 @@ import sys
 from tqdm import tqdm
 
 working_path = sys.argv[1] if len(sys.argv) > 1 else pathlib.Path(".")
+ffmpeg_path = sys.argv[2] if len(sys.argv) > 1 else "ffmpeg"
 
 
 processes = []
 for mkv in tqdm(working_path.glob('**/*.mkv'), total=len(list(working_path.glob('**/*.mkv')))):
     new_path = pathlib.Path('./scrapped') / (str(mkv)[:-3]+'ass')
     new_path.parent.mkdir(parents=True, exist_ok=True)
-    subprocess.Popen(['"C:\Program Files\ffmpeg\bin\ffmpeg.exe"', '-y', '-hide_banner', '-loglevel', 'error', '-i', str(mkv.absolute()), '-map', '0:s:0', str(new_path)], stdout=subprocess.DEVNULL).wait()
+    subprocess.Popen([ffmpeg_path, '-y', '-hide_banner', '-loglevel', 'error', '-i', str(mkv.absolute()), '-map', '0:s:0', str(new_path)], stdout=subprocess.DEVNULL).wait()
 
 shutil.make_archive("SENDME", 'zip', pathlib.Path('./scrapped').absolute().__str__())
 
